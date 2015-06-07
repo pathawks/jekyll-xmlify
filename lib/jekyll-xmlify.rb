e2134e1d0779c4e6f37da6d449ffb65dc2d5da7b
Pat Hawks <pat@pathawks.com>
2015-06-07 11:24:37 -0700

    Second Commit

--- lib/jekyll-xmlify.rb
+++ lib/jekyll-xmlify.rb
@@ -1,9 +1,9 @@
 require 'redcarpet'
 
-module xmlify
+module XMLify
   def xmlify(input)
-    input.gsub(/\s*\n\s*/, " ").gsub(/>\s+>/, "><")
+    input.gsub(/\s*\n\s*/, " ").gsub(/>\s+</, "><").gsub(/(['"])\s+(\/?)>/, '\1\2>').strip
   end
 end
 
-Liquid::Template.register_filter(xmlify)
+Liquid::Template.register_filter(XMLify)
