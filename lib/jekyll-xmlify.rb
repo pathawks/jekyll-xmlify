a63c9fd98ce01da62da419f936deffbca032693a
Pat Hawks <pat@pathawks.com>
2016-10-21 22:19:24 -0500

    Oops

--- lib/jekyll-xmlify.rb
+++ lib/jekyll-xmlify.rb
@@ -2,7 +2,7 @@ require 'redcarpet'
 
 module XMLify
   def xmlify(input)
-    input.strip.gsub(%r!(?<=[^\s]\s)\s+(?=[^\s])|(?<=>)\s+(?=<)|\s+(?=\\>)!, "".freeze)
+    input.strip.gsub(%r!\s+!," ".freeze).gsub(%r!(?<=\S\s|\A)\s+(?=\S|\Z)|(?<=>)\s+(?=<)|\s+(?=/?>)!, "".freeze)
   end
 end
 
